e72cc17c14d67358600bd3ccd93dbe5f340183b7
iammangod96 <man.god96@gmail.com>
2017-09-13 00:51:58 +0530

    week 3 first problem added

---
+++ week_3/change.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int get_change(int m) {
+  int n = 0;
+  while(m >= 10)
+  {
+  	m -= 10;
+  	n++;
+  }
+  while(m >= 5)
+  {
+  	m -= 5;
+  	n++;
+  }
+  while(m > 0)
+  {
+  	m--;
+  	n++;
+  }
+  return n;
+}
+
+int main() {
+  int m;
+  std::cin >> m;
+  std::cout << get_change(m) << '\n';
+}
